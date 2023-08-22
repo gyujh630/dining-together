76be95e9a6a6233e77d74b7893f20dbff8ca5231
gyujh630 <rbwjrbxo@ajou.ac.kr>
2023-08-22 15:20:47 +0900

    Refactor: 구조 변경

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,21 +1,69 @@
+import { Request, Response } from 'express';
 import { Connection } from 'mysql2/promise';
 import { User } from '../models/userModel';
+import * as userService from '../services/userService';
 
-export async function createUser(connection: Connection, user: User) {
+// 회원 생성
+export async function createUser(
+  req: Request,
+  res: Response,
+  connection: Connection
+) {
   try {
-    const createUserQuery = `
-            INSERT INTO USER (email, password, name, phoneNum, userType)
-            VALUES (?, ?, ?, ?, ?)
-        `;
+    const user: User = req.body;
+    const createdUser = await userService.createUser(connection, user);
+    res.status(201).json(createdUser);
+  } catch (error: any) {
+    res.status(500).send(`Error: ${error.message}`);
+  }
+}
 
-    await connection.query(createUserQuery, [
-      user.email,
-      user.password,
-      user.name,
-      user.phoneNum,
-      user.userType,
-    ]);
-  } catch (error) {
-    throw new Error(`Error inserting user: ${error}`);
+// 회원 조회
+export async function getUserById(
+  req: Request,
+  res: Response,
+  connection: Connection
+) {
+  try {
+    const userId: number = parseInt(req.params.userId, 10);
+    const user = await userService.getUserById(connection, userId);
+    if (!user) {
+      res.status(404).send('User not found');
+    } else {
+      res.json(user);
+    }
+  } catch (error: any) {
+    res.status(500).send(`Error: ${error.message}`);
+  }
+}
+
+// 회원 수정
+export async function updateUserById(
+  req: Request,
+  res: Response,
+  connection: Connection
+) {
+  try {
+    const userId: number = parseInt(req.params.userId, 10);
+    const updatedUser: User = req.body;
+    await userService.updateUserById(connection, userId, updatedUser);
+    res.send('User updated successfully');
+  } catch (error: any) {
+    res.status(500).send(`Error: ${error.message}`);
+  }
+}
+
+// 회원 삭제
+export async function deleteUserById(
+  req: Request,
+  res: Response,
+  connection: Connection
+) {
+  try {
+    const userId: number = parseInt(req.params.userId, 10);
+    await userService.deleteUserById(connection, userId);
+    res.send('User deleted successfully');
+  } catch (error: any) {
+    res.status(500).send(`Error: ${error.message}`);
   }
 }
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,11 +1,12 @@
 export interface User {
   userId: number;
+  externalId: string;
   email: string;
   password: string;
   name: string;
   phoneNum: string;
-  userType: number;
-  createAt: Date;
+  userType: number; //0 -> 관리자. 1 -> 일반. 2-> 사장님
+  createdAt: Date;
   modifiedAt: Date;
   isDeleted: boolean;
 }
